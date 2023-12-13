c07a7af0ab24dd30dab6be7d4fe1c3b504d66c50
Your Name <mangeshohol07@gmail.com>
2023-12-13 00:40:49 -0800

    commit

---
+++ Demo1/src/IfDemo.java
@@ -0,0 +1,16 @@
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		
+	int no= 12;
+	
+	if((no%2)==0) {
+		
+		System.out.println("no="+no+"is an Even number");
+	}
+}
+}
+	
+
+
